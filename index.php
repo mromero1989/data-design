f1d164dbda8c33fcd5c3e16039b3bd874ad43064
Michael Romero <m.romero1989@gmail.com>
2017-10-13 10:12:41 -0600

    I made a framework of html and added Persona and User story information.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+<HTML lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<h2>Persona</h2>
+		<P>Reddit caters to people who enjoy constant news updates and to discuss a myriad of topics in a forum.
+		   <br/>
+			Early adopters are younger and mostly men. According to statistics from Google Ad planner, 75% of the users are male.
+			<br/>
+			Also, according to Pew Research, user are also younger than average with less than 1% being over 65.
+		</P>
+		<h2>User Story</h2>
+		<p>As a user I want to I want see news updates and to discuss many topics.</p>
+		<h2>User Case/Interaction flow</h2>
+		<p></p>
+		<h2>Conceptual Model</h2>
+		<p></p>
+	</body>
+</HTML>
