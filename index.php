e4e7ad90d8133ddfdcf22596ec5e94595ecf74f9
Michael Romero <m.romero1989@gmail.com>
2017-10-15 11:14:22 -0600

    Added user case and interaction flow.

--- index.php
+++ index.php
@@ -7,16 +7,33 @@
 	<body>
 		<h1>Data Design Project</h1>
 		<h2>Persona</h2>
-		<P>Reddit caters to people who enjoy constant news updates and to discuss a myriad of topics in a forum.
+		<P>Reddit provides a service to people who enjoy constant news updates and to discuss a myriad of topics in a forum.
 		   <br/>
 			Early adopters are younger and mostly men. According to statistics from Google Ad planner, 75% of the users are male.
 			<br/>
-			Also, according to Pew Research, user are also younger than average with less than 1% being over 65.
+			Also, according to Pew Research, users are younger than average with less than 1% being over 65.
 		</P>
 		<h2>User Story</h2>
-		<p>As a user I want to I want see news updates and to discuss many topics.</p>
-		<h2>User Case/Interaction flow</h2>
-		<p></p>
+		<p>As a user I want to see news updates and to discuss many topics.</p>
+		<h2>User Case</h2>
+		<ul>
+			<li>Title: Reddit </li>
+			<li>Scenerio: Trending news, subs for any topics, discussion forums.</li>
+			<li>Redditors: Users who read, subscribe, and contribute.</li>
+		</ul>
+		<h2>Interaction Flow</h2>
+		<ol>
+			<li>I search for a specific subreddit through search bar.</li>
+			<li>The site loads many subreddits and displays them.</li>
+			<li>I click on subreddit.</li>
+			<li>The site loads trending topics.</li>
+			<li>I click on a post.</li>
+			<li>The site loads the post and displays it.</li>
+			<li>I click the button to comment.</li>
+			<li>The site displays a field to type comment.</li>
+			<li>I click submit.</li>
+			<li>The site saves comment and displays it in the thread.</li>
+		</ol>
 		<h2>Conceptual Model</h2>
 		<p></p>
 	</body>
