d1075f8287896262fe8ae0df59b7d9d5a14d950f
Michael Romero <m.romero1989@gmail.com>
2017-10-16 16:46:49 -0600

    Added more info to persona.

--- index.php
+++ index.php
@@ -8,37 +8,53 @@
 	<body>
 		<h1>Data Design Project</h1>
 		<h2>Persona</h2>
-		<P>Reddit provides a service to people who enjoy constant news updates and to discuss a myriad of topics in a forum.
-		   <br/>
-			Early adopters are younger and mostly men. According to statistics from Google Ad planner, 75% of the users are male.
-			<br/>
-			Also, according to Pew Research, users are younger than average with less than 1% being over 65.
-		</P>
+		<P>Adam is a 28 year old male. He likes to be updated about the most recent news from around the world. He also really loves discussing and reading about music. His needs are to be able to post about the topics he finds interesting. He also needs to be able to comment on others posts. Ideal features Adam likes are multi platform accessing, the ability to share posts to others on social media, and to subscribe to certain subreddits. He uses an iMac that runs the most recent MacOS update and an iPhone 6 Plus with the latest iOS update. He gets frustrated by ads and click bait. He also doesn't like trollers and users who link with malware. </P>
 		<h2>User Story</h2>
 		<p>As a user I want to see news updates and to discuss many topics.</p>
 		<h2>User Case</h2>
 		<ul>
 			<li>Title: Reddit </li>
-			<li>Scenerio: Trending news, subs for any topics, discussion forums.</li>
+			<li>Scenerio: Adam wants to comment on a certain post.</li>
 			<li>Redditors: Users who read, subscribe, and contribute.</li>
 		</ul>
 		<h2>Interaction Flow</h2>
 		<ol>
-			<li>I search for a specific subreddit through search bar.</li>
+			<li>Adam logs in to account.</li>
+			<li>The site logs Adam in.</li>
+			<li>Adam searches for a specific subreddit through the search bar.</li>
 			<li>The site loads many subreddits and displays them.</li>
-			<li>I click on subreddit.</li>
-			<li>The site loads trending topics.</li>
-			<li>I click on a post.</li>
+			<li>Adam clicks on a subreddit.</li>
+			<li>The site loads trending posts.</li>
+			<li>Adam clicks on a post.</li>
 			<li>The site loads the post and displays it.</li>
-			<li>I click the button to comment.</li>
+			<li>Adam clicks the button to comment.</li>
 			<li>The site displays a field to type comment.</li>
-			<li>I click submit.</li>
+			<li>Adam clicks submit.</li>
 			<li>The site saves comment and displays it in the thread.</li>
 		</ol>
 		<h2>Conceptual Model</h2>
+		<h2>Profile</h2>
 		<ul>
-			<li>One user can post many times.</li>
-			<li>Many users can comment on many posts.</li>
+			<li>profileId</li>
+			<li>profileActivationToken</li>
+			<li>profileAtHandle</li>
+			<li>profileEmail</li>
+			<li>profileHash</li>
+			<li>profilePhone</li>
+			<li>profileSalt</li>
+		</ul>
+		<h2>Comment</h2>
+		<ul>
+			<li>commentId</li>
+			<li>commentProfileId</li>
+			<li>commentContent</li>
+			<li>commentDate</li>
+		</ul>
+		<h2>Relations</h2>
+		<ul>
+			<li>One user can post many times.(1-n)</li>
+			<li>One user can comment on many posts. (1-n)</li>
+			<li>One post can have many comments. (1-n)</li>
 		</ul>
 		<p></p>
 	</body>
