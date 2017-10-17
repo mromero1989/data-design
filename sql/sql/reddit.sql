883593977e1e7605248dd7d1da3fda64a4ba0e2f
Michael Romero <m.romero1989@gmail.com>
2017-10-16 20:33:53 -0600

    Created statement one for profile.

---
+++ sql/sql/reddit.sql
@@ -0,0 +1,4 @@
+INSERT INTO profile(profileId, profileActivationToken, profileAtHandle, profileEmail, profileHash, profilePhone, profileSalt)
+	VALUES(UNHEX(REPLACE("e34cb088-3d83-4620-a363-5d1a10617379", "-", "")),
+
+	);
