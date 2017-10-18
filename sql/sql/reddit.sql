3e85968fbe85b3ab1328182a9c9dcd73cf21236f
Michael Romero <m.romero1989@gmail.com>
2017-10-18 16:41:56 -0600

    php/classes/Foo.php file

--- sql/sql/reddit.sql
+++ sql/sql/reddit.sql
@@ -21,10 +21,8 @@ CREATE TABLE comment (
 	PRIMARY KEY(commentId)
 );
 
-INSERT INTO
-	profile(profileId, profileActivationToken, profileAtHandle, profileEmail, profileHash, profilePhone, profileSalt)
-	VALUES
-		(UNHEX(REPLACE("e34cb088-3d83-4620-a363-5d1a10617379", "-", "")), "b8ae3c450969e13601b1b47aa6a7e488", "mromero308", "mromero308@cnm.edu", "854d1539fe105bf35aacb1ac8f5c0882acfa9a109a3664e4002a7f279460b54b158dca5f6a62d859925523279fcd7ae7cca4c53d4b071ea07ad28b508884adf4", "+5059171406", "3650b6eac8d67f6b02eb629939daf1c00c642758059769ac28ef67ff71d91c90"
+INSERT INTO profile(profileId, profileActivationToken, profileAtHandle, profileEmail, profileHash, profilePhone, profileSalt)
+	VALUES (UNHEX(REPLACE("e34cb088-3d83-4620-a363-5d1a10617379", "-", "")), "b8ae3c450969e13601b1b47aa6a7e488", "mromero308", "mromero308@cnm.edu", "854d1539fe105bf35aacb1ac8f5c0882acfa9a109a3664e4002a7f279460b54b158dca5f6a62d859925523279fcd7ae7cca4c53d4b071ea07ad28b508884adf4", "+5059171406", "3650b6eac8d67f6b02eb629939daf1c00c642758059769ac28ef67ff71d91c90"
 		);
 
 DELETE FROM profile
